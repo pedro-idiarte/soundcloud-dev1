9069ae3b1f2bb965f6f6f32f3457336c94bb98f4
Pedro Idiarte Alves <pedroidiarte7@gmail.com>
2024-10-25 03:33:41 -0300

    first commit

---
+++ soundcloud_apnp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for soundcloud_apnp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soundcloud_apnp.settings')
+
+application = get_asgi_application()
